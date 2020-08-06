1d9a94e2f84c613887be706fbea00724cf5be0c3
William Ting <williamdjting@gmail.com>
2020-08-05 18:58:51 -0700

    create form

--- src/components/Appointment/Form.js
+++ src/components/Appointment/Form.js
@@ -1,44 +1,110 @@
-import React, { useState } from 'react'
-
-import InterviewerList from "components/InterviewerList";
-
-import Button from "components/Button";
-
-
+import React from "react"
+import Button from "components/Button"
+import InterviewerList from "components/InterviewerList"
 export default function Form(props) {
-  const [name, setName] = useState(props.name || "");
-  const [interviewer, setInterviewer] = useState(props.interviewer || null);
-
+  const [name, setName] = React.useState(props.name || null);
+  const [interviewer, setInterviewer] = React.useState(props.interviewer || null);
+  const [error, setError] = React.useState("");
+  const reset = () => {setName(""); setInterviewer(null);};
+  const cancel = () => {props.onCancel(reset())};
+  const validate = () => {
+    if (name === "") {
+      setError("Please enter your name");
+      return;
+    } else {
+      setError("");
+      props.onSave(name, interviewer)
+    }
+  }
   return (
     <main className="appointment__card appointment__card--create">
       <section className="appointment__card-left">
-        <form autoComplete="off">
+        <form onSubmit={event => event.preventDefault()} autoComplete="off">
           <input
             className="appointment__create-input text--semi-bold"
             value={name}
-            onChange={(event) => setName(event.target.value)}
-            name={props.name}
             type="text"
             placeholder="Enter Student Name"
-          /*
-            This must be a controlled component
-          */
+            onChange={(event) => setName(event.target.value)}
+            /*
+              This must be a controlled component
+            */
           />
         </form>
-        <InterviewerList interviewers={props.interviewers} value={interviewer} onChange={setInterviewer} />
+        <section className="appointment__validation">{error}</section>
+        <InterviewerList 
+        interviewers={props.interviewers} 
+        value={interviewer} 
+        onChange={(event) => setInterviewer(event)} />
       </section>
       <section className="appointment__card-right">
         <section className="appointment__actions">
-          <Button danger onClick={props.onCancel onChange={()}}>Cancel</Button>
-          <Button confirm onClick={props.onSave}>Save</Button>
+              <Button onClick={cancel} danger>Cancel</Button>
+              <Button onClick={validate} confirm>Save</Button>
         </section>
       </section>
     </main>
-  );
-
-  const reset = function(setName(""), setInterviewer(null)) {}
-
-  const cancel = function(reset(), props.onCancel
-
+  )
 }
+
+// import React, { useState } from 'react'
+
+// import InterviewerList from "components/InterviewerList";
+
+// import Button from "components/Button";
+
+
+// export default function Form(props) {
+//   const [name, setName] = useState(props.name || "");
+//   const [interviewer, setInterviewer] = useState(props.interviewer || null);
+//   const [error, setError] = React.useState("");
+
+//   const reset = function() {
+//     setName("");
+//     setInterviewer(null);
+//   }
+
+//   const cancel = function() {
+//     props.onCancel(reset())
+//   }
+
+//   const validate = () => {
+//     if (name === "") {
+//       setError("Please enter your name");
+//       return;
+//     } else {
+//       setError("");
+//       props.onSave(name, interviewer)
+//     }
+//   }
+
+  
+//   return (
+//     <main className="appointment__card appointment__card--create">
+//       <section className="appointment__card-left">
+//         <form autoComplete="off" onSubmit={event => event.preventDefault()}>
+//           <input
+//             className="appointment__create-input text--semi-bold"
+//             value={name}
+//             onChange={(event) => setName(event.target.value)}
+//             // name={props.name}
+//             type="text"
+//             placeholder="Enter Student Name"
+//           /*
+//             This must be a controlled component
+//           */
+//           />
+//         </form>
+//         <section className="appointment__validation">{error}</section>
+//         <InterviewerList interviewers={props.interviewers} value={interviewer} onChange={(event) => setInterviewer(event)} />
+//       </section>
+//       <section className="appointment__card-right">
+//         <section className="appointment__actions">
+//           <Button danger onClick={cancel()}>Cancel</Button>
+//           <Button onClick={validate} confirm>Save</Button>
+//         </section>
+//       </section>
+//     </main>
+//   );
+// }
 
--- stories/index.js
+++ stories/index.js
@@ -35,6 +35,8 @@ import Status from "components/Appointment/Status"
 
 import Error from "components/Appointment/Error"
 
+import Form from "components/Appointment/Form"
+
 storiesOf("Button", module)
   .addParameters({
     backgrounds: [{ name: "dark", value: "#222f3e", default: true }]
@@ -192,20 +194,38 @@ storiesOf("Appointment", module)
       onClose={action("onClose")}
     />
   ))
-  .add("Edit", () => (
-    <Edit
-      name=" "
-      interviewers={interviewers}
-      interviewer={2}
-      onSave={action("onSave")}
-      onCancel={action("onCancel")}
-    />
-  ))
   .add("Create", () => (
-    <Create
+    <Form
       interviewers={interviewers}
       onSave={action("onSave")}
       onCancel={action("onCancel")}
     />
   ))
+  .add("Edit", () => (
+    <Form
+      name="Archie Cohen"
+      interviewers={interviewers}
+      // interviewer={interviewers[0].id}
+      interviewer={interviewer.id}
+      onSave={action("onSave")}
+      onCancel={action("onCancel")}
+    />
+  ))
+
+  // .add("Edit", () => (
+  //   <Edit
+  //     name=" "
+  //     interviewers={interviewers}
+  //     interviewer={2}
+  //     onSave={action("onSave")}
+  //     onCancel={action("onCancel")}
+  //   />
+  // ))
+  // .add("Create", () => (
+  //   <Create
+  //     interviewers={interviewers}
+  //     onSave={action("onSave")}
+  //     onCancel={action("onCancel")}
+  //   />
+  // ))
   
