5448cbd736ab2be021cc75887c0ff21818b4c939
William Ting <williamdjting@gmail.com>
2020-08-31 19:31:31 -0700

    fixed use applicaiton data comment

--- src/hooks/useApplicationData.js
+++ src/hooks/useApplicationData.js
@@ -39,7 +39,7 @@ export default function useApplicationData() {
               ...state,
               appointments,
               days
-            // });
+            });
             // console.log(response)
           })
     )
